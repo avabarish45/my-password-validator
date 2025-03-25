4788dd0735c8f7e11d5b7141da33268bd037a670
avabarish45 <avab@sas.upenn.edu>
2025-03-25 13:21:23 -0400

    adding tests

--- test_main.py
+++ test_main.py
@@ -31,3 +31,47 @@ def test_check_password_accurate_password(client):
     assert resp.status_code == 200
     data = resp.get_json()
     assert data.get("valid") is True
+
+def test_check_password_no_digit(client):
+    """
+    Test the POST '/v1/checkPassword' endpoint to ensure
+    it returns true when password is valid
+    """
+    resp = client.post("/v1/checkPassword", json={"password": "Password!"})
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert data.get("valid") is False
+    assert data.get("reason") == "No digit"
+
+def test_check_password_no_upper(client):
+    """
+    Test the POST '/v1/checkPassword' endpoint to ensure
+    it returns true when password is valid
+    """
+    resp = client.post("/v1/checkPassword", json={"password": "password1!"})
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert data.get("valid") is False
+    assert data.get("reason") == "No uppercase letter"
+
+def test_check_password_no_special(client):
+    """
+    Test the POST '/v1/checkPassword' endpoint to ensure
+    it returns true when password is valid
+    """
+    resp = client.post("/v1/checkPassword", json={"password": "Password1"})
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert data.get("valid") is False
+    assert data.get("reason") == "No special character"
+
+def test_check_password_too_short(client):
+    """
+    Test the POST '/v1/checkPassword' endpoint to ensure
+    it returns true when password is valid
+    """
+    resp = client.post("/v1/checkPassword", json={"password": "P1!"})
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert data.get("valid") is False
+    assert data.get("reason") == "Too short"
